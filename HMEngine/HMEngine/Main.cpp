2b1cda6da544d98c1faac5308c6b5acf79ad642a
harel <harel.finaly@gmail.com>
2017-11-30 19:13:18 +0200

    little fixes

--- HMEngine/HMEngine/Main.cpp
+++ HMEngine/HMEngine/Main.cpp
@@ -5,14 +5,9 @@ int main()
 	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) //initialize SDL
 	{
 		std::cout << "Succeeded initializing SDL..." << std::endl;
-<<<<<<< HEAD
+
 		std::string title = "test";
-		HMEngine::Core::Rendering::Render display(800, 600, title, 0); //create window
-=======
 
-		std::string title = "test";		
-
->>>>>>> Organizing-Rendering
 		HMEngine::GameEngine g;
 		g.CreateNewWindow(800, 600, title, false);
 		
